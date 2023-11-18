289e257a5fd2ba157d40d9ac306b783ebcd749b1
Adjai87 <adjai87@gmail.com>
2023-11-19 00:56:16 +0200

    first codes

---
+++ 2_variable.dart
@@ -0,0 +1,9 @@
+int main(){
+
+var name = "Adjai";
+var age = 35;
+var district = "Mzimba";
+
+print("My name is ${name}, I am ${age}, and I come from ${district}");
+  
+  return 0;}
---
+++ 3_operators.dart
@@ -0,0 +1,12 @@
+int main(){
+
+for(int a = 0; a <=10; a++){
+
+print(a);
+
+}
+
+
+
+  return 0;
+  }
