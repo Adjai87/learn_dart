f5e2ef42ffdf5614c392f19226c80397a16133b1
Adjai87 <adjai87@gmail.com>
2023-11-19 15:46:56 +0200

    Create 9_switch_statement.dart

---
+++ 9_switch_statement.dart
@@ -0,0 +1,32 @@
+void main(){
+
+   int grade = 90;
+
+   switch(grade){
+    
+    case 70:{
+     
+     print("C");
+
+    } break;
+
+    case 80:{
+     
+     print("B");
+
+    } break;
+
+    case 90:{
+     
+     print("A");
+
+    } break;
+default: { 
+        print("This is default case"); 
+    } break;
+
+
+
+   }
+
+}
