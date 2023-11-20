39cf2dd2265a40560b6e64d921584f55f8a02feb
Adjai87 <adjai87@gmail.com>
2023-11-20 10:42:32 +0200

    Create 13_returning_functions.dart

---
+++ 13_returning_functions.dart
@@ -0,0 +1,19 @@
+ int main(){
+
+  int zaka = cal_age(1987);
+  print("My name is Adjai, my age is $zaka ");
+
+return 0;
+
+ }
+
+ int cal_age(int dob){
+
+   int thisyear = 2023;
+
+   int diff = thisyear - dob;
+
+   return diff;
+
+
+ }
